4644fe807598f64dcf4b960a1e24066f4301ff15
mithranivetha <mithranivetha07@gmail.com>
2026-05-08 12:38:10 +0400

    Day 2: Added Clerk auth and Prisma database

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/communities(.*)",
+  "/r/(.*)",
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
