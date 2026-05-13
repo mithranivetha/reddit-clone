d33c649e7f38940c9db16fe51f7c42b94f11f465
mithranivetha <mithranivetha07@gmail.com>
2026-05-13 10:17:47 +0400

    Fix: next.config and bookmarks route

--- app/api/bookmarks/all/route.js
+++ app/api/bookmarks/all/route.js
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic";
-export const runtime = "nodejs";
+export const fetchCache = "force-no-store";
 
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
