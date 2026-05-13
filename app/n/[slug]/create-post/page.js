4d022149d3cc0cae4e35fc31b6fdc5ccf1f7523e
mithranivetha <mithranivetha07@gmail.com>
2026-05-13 09:51:45 +0400

    Fix: Add force-dynamic to all pages

--- app/n/[slug]/create-post/page.js
+++ app/n/[slug]/create-post/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import CreatePostForm from "@/components/CreatePostForm";
 import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
--- app/profile-setup/page.js
+++ app/profile-setup/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import ProfileSetupForm from "@/components/ProfileSetupForm";
 
 export default function ProfileSetupPage() {
