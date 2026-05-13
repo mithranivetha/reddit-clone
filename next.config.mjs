3c211b6122fc4de1d79f3bcc7c9e5dc1a0f61d8b
mithranivetha <mithranivetha07@gmail.com>
2026-05-13 11:39:29 +0400

    Fix: Remove dataproxy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: "standalone",
 };
 
 export default nextConfig;
